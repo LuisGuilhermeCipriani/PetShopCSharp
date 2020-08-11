a307a1c4b961200c5e9a5700ecc1cccbeb3157ca
LuisGuilhermeCipriani <cipriani.luisguilherme@gmail.com>
2020-08-11 14:55:43 -0300

    Criação da Classe AnimalController

---
+++ controller/AnimalController.cs
@@ -0,0 +1,30 @@
+﻿using PetShopCSharp.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopCSharp.controller
+{
+    class AnimalController
+    {
+        private List<Animal> listaAnimais;
+
+        public AnimalController()
+        {
+            listaAnimais = new List<Animal>();
+        }
+
+        public void cadastrarAnimal()
+        {
+            Animal animal = new Animal();
+            listaAnimais.Add(animal);
+        }
+
+        public List<Animal> getListaAnimais()
+        {
+            return listaAnimais;
+        }
+    }
+}
