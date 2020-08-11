f6215d4ff9642215debb04f8680cb2a685bb0eaf
LuisGuilhermeCipriani <cipriani.luisguilherme@gmail.com>
2020-08-11 13:49:48 -0300

    Criação da Classe Cliente

---
+++ model/Cliente.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopCSharp.model
+{
+    class Cliente
+    {
+        private int id;
+        private string nome;
+        private string cpf;
+        private int idade;
+
+        public Cliente()
+        {
+
+        }
+
+        public Cliente(string nome, string cpf, int idade)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.idade = idade;
+        }
+
+        
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public void setCpf()
+        {
+            this.cpf = cpf;
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getCpf()
+        {
+            return cpf;
+        }
+        public int getIdade()
+        {
+            return idade;
+        }
+
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+    }
+}
